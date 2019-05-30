2d7ba8609f82799d2eb59157a51a0f8612e42bf3
Susmita21197 <choudhurysusmita70>
2019-05-30 04:27:12 -0700

    update

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,13 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e=new Employee();
+		e.setEmpId(345);
+		e.setEmpName("dfgh");
+		e.setEmpSal(98698);
+
+	}
+
+}
